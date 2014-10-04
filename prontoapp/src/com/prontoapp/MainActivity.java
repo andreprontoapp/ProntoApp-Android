2068eddb446847afd2a2d425aebad01f1ca6df26
André Luis <andre@prontoapp.com.br>
2014-10-04 20:44:18 -0300

    Primeiro commit

---
+++ prontoapp/src/com/prontoapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.prontoapp;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+	private WebView mainWebView;
+	@SuppressLint("SetJavaScriptEnabled")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		
+		mainWebView = (WebView) findViewById(R.id.webViewMain);
+		mainWebView.setWebViewClient(new WebViewClient());
+		mainWebView.getSettings().setJavaScriptEnabled(true);
+		mainWebView.loadUrl("http://192.168.0.101/prontoapp");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
